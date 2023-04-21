24d08f59c4c8cd9c776bb13b55e76e8b38b02848
Mármarosi József <marmarosi@logikum.hu>
2023-04-21 17:09:35 +0200

    feat: add contract snippets

---
+++ Csla6RestApi.Snippets/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Csla6RestApi.Snippets
+{
+    // $RootName$   $ChildName$
+    // $RootModel$  $ChildModel$  $ParentModel$
+    // $RootParam$  $ChildParam$
+
+    
+}
